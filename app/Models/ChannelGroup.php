7dae196dbadc88042189b1b9ae6e353b72a086cd
Admin <admin@tudominio.com>
2026-04-23 18:00:46 -0300

    fix: agregar playlist_id a fillable en ChannelGroup

--- app/Models/ChannelGroup.php
+++ app/Models/ChannelGroup.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ChannelGroup extends Model
 {
-    protected $fillable = ['name', 'slug', 'type', 'external_id', 'source_id'];
+    protected $fillable = ['name', 'slug', 'type', 'external_id', 'source_id', 'playlist_id', 'is_adult'];
 
     public function channels()
     {
