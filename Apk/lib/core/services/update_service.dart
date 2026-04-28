6f77c023afe675cf4bcd6fab9f64b9e51fe328a2
Admin <admin@tudominio.com>
2026-04-28 17:54:53 -0300

    fix: agregando import faltante para instalador

--- Apk/lib/core/services/update_service.dart
+++ Apk/lib/core/services/update_service.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:dio/dio.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../network/dio_client.dart';
 import '../../core/theme/app_theme.dart';
 
